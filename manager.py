aff7319dece7c2dd0b96b2c61d8400424ab8d8aa
shonenada <shonenada@gmail.com>
2013-09-29 21:34:38 +0800

    Added developing tools.

--- manager.py
+++ manager.py
@@ -1,13 +1,23 @@
 from flask.ext.script import Manager
 
 from szufm.app import app
+from develop_tools.clean import clean as cln
+from develop_tools.pep8 import pep8
 
 
 application = app('szufm', 'development.conf')
 application.debug = True
-
-
 manager = Manager(application)
+
+
+@manager.command
+def clean():
+    cln
+
+
+@manager.command
+def check():
+    pep8()
 
 
 if __name__ == '__main__':
